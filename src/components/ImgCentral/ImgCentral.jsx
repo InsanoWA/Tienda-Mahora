015768b6addd9b6f3afa194a113c021850941cd9
MIGUELANGEL3924 <mkitito2484@gmail.com>
2023-05-02 12:54:10 -0500

    actualizando imagen

---
+++ src/components/ImgCentral/ImgCentral.jsx
@@ -0,0 +1,8 @@
+const ImgCentral = () => {
+    return(
+        <img class="mx-auto"
+        src="https://lh3.googleusercontent.com/p/AF1QipMu9j5s1dMmcNerufTYuABvjIwdarLEtSMxHTLC=w1080-h608-p-no-v0" ></img>
+    )
+}
+
+export default ImgCentral;
