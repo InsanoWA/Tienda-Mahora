d4f41fe743999a2e672bcfde213e000fab1f9719
MIGUELANGEL3924 <mkitito2484@gmail.com>
2023-05-02 12:51:31 -0500

    actualizando comunicarse

---
+++ src/components/Comunicarse/Comunicarse.jsx
@@ -0,0 +1,15 @@
+const Comunicarse = () => {
+    return (
+        <>
+        <div id="contactocd" >
+        <h1  className="flex justify-center">COMUNÍCATE CON NOSOTROS</h1>
+            <br />
+            <img class="mx-auto" src="https://www.sugoi.com.pe//wp-content/uploads/2018/09/mapa-utp.png" alt="image description"></img>
+        </div>
+            
+        </>
+
+    )
+}
+
+export default Comunicarse;
