5a8e6f38e0259a5b2445aa77383a49b265e10553
MIGUELANGEL3924 <mkitito2484@gmail.com>
2023-05-03 20:36:32 -0500

    creando id seccion5

---
+++ src/components/Informacion/index.jsx
@@ -0,0 +1,12 @@
+const Informacion = () =>{
+    return(
+        <div id="seccion5">
+            <h1 className="flex justify-center">TIENDA DE ANIME</h1>
+          <p className="flex justify-center">Contamos con productos y merchandising de anime clasicos y nuevos, personalizamos tazas por pedido,<br/> 
+                consultas a nuestro whatsap 946314456</p>
+        </div>
+
+    )
+}
+
+export default Informacion;
