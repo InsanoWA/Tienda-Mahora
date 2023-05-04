8bcaea4d0ca125da1b351ffe97da598a2d57d119
MIGUELANGEL3924 <mkitito2484@gmail.com>
2023-05-03 20:30:21 -0500

    agregando id contacto

---
+++ src/components/Comunicarse/index.jsx
@@ -0,0 +1,17 @@
+const Comunicarse = () => {
+    return (
+        
+        <div id="contacto">
+        <h1  className="flex justify-center">COMUNÍCATE CON NOSOTROS</h1>
+            <br />
+            <img 
+            class="mx-auto" 
+            src="https://www.sugoi.com.pe//wp-content/uploads/2018/09/mapa-utp.png" alt="image description"></img>
+        </div>
+            
+        
+
+    )
+}
+
+export default Comunicarse;
