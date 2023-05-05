2b7828057bc7467baf1aa711a64b4c5b67246bf3
MIGUELANGEL3924 <mkitito2484@gmail.com>
2023-05-04 22:23:27 -0500

    cambiando tamaño de texto

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,30 +1,24 @@
+
+
 const Header = () => {
     return (
         <div className="w-full bg-white rounded-lg p-4 flex flex-col gap-4 items-center justify-center md:justify-between">
-          <nav className="flex justify-center gap-4">
-                <a
-                    href="#"
-                    className="xl:py-1 xl:px-2 rounded-lg hover:bg-gray-100 transition-colors">
-                    Pedir Una Cotizacion
-                </a>
+            <nav className="flex justify-center gap-4">
                 <a
                     href="tel:+51-963-746-684"
-                    className="xl:py-1 xl:px-2 rounded-lg hover:bg-gray-100 transition-colors "
+                    className=" font-mono  text-lime-950 text-xl   xl:py-1 xl:px-2 rounded-lg hover:bg-gray-100 transition-colors "
                 >
-                    Llamar Ahora
+                    LLAMAR AHORA
                 </a>
                 <a
                     href="https://www.google.com/maps/@-12.2413093,-76.9290612,21z"
-                    className="xl:py-1 xl:px-2 rounded-lg hover:bg-gray-100 transition-colors"
+                    className=" font-mono text-lime-950 text-xl  xl:py-1 xl:px-2 rounded-lg hover:bg-gray-100 transition-colors"
                 >
-                    Como Llegar
+                    COMO LLEGAR
                 </a>
-               
             </nav>
         </div>
-
     )
 }
-
 
 export default Header;
