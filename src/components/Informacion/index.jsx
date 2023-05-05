2a18e087024c456ad6b048aca4d42b6e00236131
MIGUELANGEL3924 <mkitito2484@gmail.com>
2023-05-04 22:32:06 -0500

    cambiando estilos de texto

--- src/components/Informacion/index.jsx
+++ src/components/Informacion/index.jsx
@@ -1,8 +1,8 @@
-const Informacion = () =>{
-    return(
+const Informacion = () => {
+    return (
         <div id="seccion5">
-            <h1 className="flex justify-center">TIENDA DE ANIME</h1>
-          <p className="flex justify-center">Contamos con productos y merchandising de anime clasicos y nuevos, personalizamos tazas por pedido,<br/> 
+            <h1 className=" font-mono  text-lime-950 text-xl antialiased underline   flex justify-center">TIENDA DE ANIME</h1>
+            <p className=" text-lg  text-black flex justify-center">Contamos con productos y merchandising de anime clasicos y nuevos, personalizamos tazas por pedido,<br />
                 consultas a nuestro whatsap 946314456</p>
         </div>
 
