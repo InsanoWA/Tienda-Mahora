7db34dcafcafe1578372f1b7b810a68865b53ad9
MIGUELANGEL3924 <mkitito2484@gmail.com>
2023-05-04 23:12:28 -0500

    modificando extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{html,js,ts,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'negima':"url('https://www.animenewsnetwork.com/images/encyc/A10713-3.jpg')",
+      }
+    },
   },
   plugins: [],
 }
