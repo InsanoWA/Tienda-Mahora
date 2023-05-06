cd7d5ed24dd1b3e6994fa718b2fef0b08ccbd250
MIGUELANGEL3924 <mkitito2484@gmail.com>
2023-05-05 20:01:18 -0500

    cambiando nombre de links

--- src/components/BotonPedir/index.jsx
+++ src/components/BotonPedir/index.jsx
@@ -7,7 +7,7 @@
         <div className="px-4 py-8 flex flex-wrap md:items-center md:justify-center gap-8" >
         <button 
         className="relative py-2 px-6 text-center text-green-600 border border-green-600 overflow-hidden transition-all ease-in-out before:absolute before:bg-green-600 before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:-z-10 before:transition-all before:duration-300 before:w-0 before:h-full hover:before:w-full hover:text-white" > 
-       <a  href="http://127.0.0.1:5500/src/components/BotonPedir/cotizacion.html">PEDIR COTIZACION</a> 
+       <a  href="http://127.0.0.1:5500/src/components/BotonPedir/index.html">PEDIR COTIZACION</a> 
         </button>
         </div>
     )
