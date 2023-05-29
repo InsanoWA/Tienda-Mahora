8a7fe414e2129dc7f69aa80dca2374f2dab6b7c8
Jorge Yupanqui <camisas.duvalier@gmail.com>
2023-05-28 21:48:24 -0500

    hola

--- src/components/Informacion/index.jsx
+++ src/components/Informacion/index.jsx
@@ -2,8 +2,7 @@ const Informacion = () => {
     return (
         <div id="seccion5">
             <h1 className=" font-mono  text-lime-950 text-xl antialiased underline   flex justify-center">TIENDA DE ANIME</h1>
-            <p className=" text-lg  text-black flex justify-center">Contamos con productos y merchandising de anime clasicos y nuevos, personalizamos tazas por pedido,<br />
-                consultas a nuestro whatsap 946314456</p>
+            <p className=" text-lg  text-black flex justify-center">946314456</p>
         </div>
 
     )
